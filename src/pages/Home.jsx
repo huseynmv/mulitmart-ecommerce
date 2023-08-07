622e1db00bcac4b70b087d38ad0d0f43cf4f735a
huseynmv <huseynmv@yahoo.com>
2023-08-07 15:18:45 +0400

    banner

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,42 @@
 import React from "react";
+import Helmet from "../components/Helmet/Helmet";
+import { Col, Container, Row } from "reactstrap";
+import heroImg from "../assets/images/hero-img.png";
+import "../styles/home.css";
+import { Link } from "react-router-dom";
+import { motion } from "framer-motion";
 
 const Home = () => {
-  return <div>Home</div>;
+  const year = new Date().getFullYear();
+  return (
+    <Helmet title={"Home"}>
+      <section className="hero__section">
+        <Container>
+          <Row>
+            <Col lg="6" md="6">
+              <div className="hero__content">
+                <p className="hero__subtitle">Trending product in {year}</p>
+                <h2>Make Your Interion More Minimalistic & Modern</h2>
+                <p>
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                  Dolores laborum quibusdam architecto sequi quasi odio rem,
+                  praesentium laudantium at officiis.
+                </p>
+                <motion.button whileTap={{ scale: 1.2 }} className="buy__btn">
+                  <Link to="/shop">SHOP NOW</Link>
+                </motion.button>
+              </div>
+            </Col>
+            <Col lg="6" md="6">
+              <div className="hero__img">
+                <img src={heroImg} alt="" />
+              </div>
+            </Col>
+          </Row>
+        </Container>
+      </section>
+    </Helmet>
+  );
 };
 
 export default Home;
