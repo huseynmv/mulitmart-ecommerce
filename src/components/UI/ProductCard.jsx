ac5b5fed7fc8d7eed2ecdc29f0ec28997390ab07
huseynmv <huseynmv@yahoo.com>
2023-08-09 17:23:47 +0400

    add to cart redux

--- src/components/UI/ProductCard.jsx
+++ src/components/UI/ProductCard.jsx
@@ -3,8 +3,25 @@ import { motion } from "framer-motion";
 import "../../styles/product-card.css";
 import { Col } from "reactstrap";
 import { Link } from "react-router-dom";
+import { useDispatch } from "react-redux";
+import { addItem } from "../../redux/slice/cartSlice";
+// import { cartActions } from "../../redux/slice/cartSlice";
 
 const ProductCard = ({ item }) => {
+  const dispatch = useDispatch();
+  const addToCart = () => {
+    dispatch(
+      addItem({
+        id: item.id,
+        productName: item.productName,
+        price: item.price,
+        image: item.imgUrl,
+      })
+    );
+
+    alert("Product added to cart");
+  };
+
   return (
     <Col lg="3" md="4" className="mb-2">
       <div className="product__item">
@@ -19,7 +36,7 @@ const ProductCard = ({ item }) => {
         </div>
         <div className="product__card-bottom d-flex align-items-center justify-content-between p-2">
           <span className="price">${item.price}</span>
-          <motion.span whileTap={{ scale: 1.2 }}>
+          <motion.span whileTap={{ scale: 1.2 }} onClick={addToCart}>
             <i class="ri-add-line"></i>
           </motion.span>
         </div>
